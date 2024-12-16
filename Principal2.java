9bf5c336b95d939c444e48c97c681a6ad53859f9
LuisBlanc98 <luisblancg@gmail.com>
2024-12-16 13:56:12 +0100

    Add files via upload

---
+++ Principal2.java
@@ -0,0 +1,34 @@
+package SQL.curso8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Principal2 {
+		
+		   static final String DB_URL = "jdbc:mysql://localhost:3306/cie1";
+		   static final String USER = "root";
+		   static final String PASS = "";
+		   static final String QUERY = "SELECT * from Personas";
+
+		   public static void main(String[] args) {
+		      // Open a connection
+		    try {
+					 Connection conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+				     Statement sentencia = conexion.createStatement();
+                     
+                     sentencia.executeUpdate("insert into personas values ('ceci','alvarez',50)");
+
+
+
+
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		   }
+		
+
+}
